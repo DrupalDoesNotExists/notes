f30d2777dc02e34ea7626af68ed045097b43395e
Dmitry Lomov <drupal.doesnot.exists@mail.ru>
2023-04-16 13:06:33 +0300

    fix: telegram workflow post: posts/now-in-telegram; Теперь в Telegram

---
+++ scripts/trim-commit-msg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Trim commit message and get new post meta
+
+# get commit message from argv
+commit_msg=$1
+meta=$(echo ${commit_msg} | sed 's/post: /post:/g' | sed 's/; /;/g' | awk -F'post:' '{print $2}')
+echo ${meta}
+
+# split meta by ; (bash-compatible ONLY!)
+values=(${meta//;/ })
+echo Slug: ${values[0]} Title: ${values[@]:1}
+
+# put github output
+echo "slug=${values[0]}" >> $GITHUB_OUTPUT
+echo "title=${values[1]}" >> $GITHUB_OUTPUT
